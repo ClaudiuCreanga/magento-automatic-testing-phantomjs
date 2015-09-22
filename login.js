56c6da8ca69fa9e4255c17919e42fba796faf5a4
Claudiu Creanga <claudiucreanga@Claudius-MacBook-Pro.local>
2015-09-22 20:37:55 +0100

    index and first logins

--- login.js
+++ login.js
@@ -1,7 +1,6 @@
 exports.go_to_login = function(){
 	casper.thenClick('ul.links > li:nth-child(3) > a:nth-child(1)',function() {
 		this.test.pass('login page was loaded');
-
-		
+		taking_screenshots("login-page");
 	});
 }
